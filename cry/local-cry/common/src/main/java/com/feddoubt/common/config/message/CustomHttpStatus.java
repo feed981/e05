f9201f0e1beed2041678c69df6d5deb663e58a95
unhappyjavamansfriend <dunhull29@gmail.com>
2025-02-10 18:30:24 +0800

    restructure cry,diary

---
+++ cry/local-cry/common/src/main/java/com/feddoubt/common/config/message/CustomHttpStatus.java
@@ -0,0 +1,27 @@
+package com.feddoubt.common.config.message;
+
+public enum CustomHttpStatus implements ApiStatus {
+    INVALID_REQUEST_DATA(400, "Invalid request data"),
+    INTERNAL_SERVER_ERROR(500,  "Internal Server Error"),
+    CONFLICT(409, "Conflict"),
+
+    ;
+
+    private final int value;
+    private final String reasonPhrase;
+
+    CustomHttpStatus(int value, String reasonPhrase) {
+        this.value = value;
+        this.reasonPhrase = reasonPhrase;
+    }
+
+    @Override
+    public int value() {
+        return value;
+    }
+
+    @Override
+    public String getReasonPhrase() {
+        return reasonPhrase;
+    }
+}
---
+++ cry/local-cry/model/src/main/java/com/feddoubt/model/dtos/CryDto.java
@@ -0,0 +1,11 @@
+package com.feddoubt.model.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class CryDto {
+    String itemname;
+    String username;
+    String password;
+}
